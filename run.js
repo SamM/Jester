8696847bacfee439493ab8f9845eb577773da54d
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2015-12-06 17:47:11 +1300

    Made bot add Botdom to auto join channels if none are configured

--- run.js
+++ run.js
@@ -1,7 +1,10 @@
 var Jester = require("./Jester");
 var BOT = new Jester();
 
-BOT.autojoin("Botdom");
+var aj = BOT.config("autojoin");
+if(!aj || Array.isArray(aj) && !aj.length){
+  BOT.autojoin("Botdom");
+}
 
 BOT.DEBUG = false;
 BOT.run();
